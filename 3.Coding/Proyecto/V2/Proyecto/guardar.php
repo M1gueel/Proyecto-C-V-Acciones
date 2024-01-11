603bbe6f646c995f45f6600d2dddcd1790491856
M1gueel <124790807+M1gueel@users.noreply.github.com>
2024-01-11 17:25:29 -0500

    Se agrega el código de la aplicación
    Se agrega el código de la aplicación

---
+++ 3.Coding/Proyecto/V2/Proyecto/guardar.php
@@ -0,0 +1,12 @@
+function agregarFila() {
+    // Lógica para agregar una nueva fila a la tabla
+}
+
+function eliminarUltimaFila() {
+    // Lógica para eliminar la última fila de la tabla
+}
+
+function guardarDatos() {
+    // Lógica para guardar los datos en el servidor o realizar otras acciones necesarias
+    // Puedes utilizar AJAX para enviar los datos al servidor si es necesario
+}
